512ccfc5694b338c87a370d5746243a5b4b18101
Wences81 <zhylinpochta@gmail.com>
2021-10-21 20:07:23 +0200

    start5

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-fetch('http://sitelicon.eu/test/ajax_localidades.php')
-    .then(res => res.json())
-    .then(json => {
+const BASE_URL = 'http://sitelicon.eu/test/ajax_localidades.php';
 
+function fetchProvincias(optoin) {
+    return fetch(`${BASE_URL}/option/${option}`).then((response) =>
+        response.json(),
+    );
+    
+
+}
         let jsObj = json
         for (let key in jsObj) {
 
-            let provinciasOption = document.createElement('option');
-            let provinciasSelect = document.getElementById('provincias');
+        let provinciasOption = document.createElement('option');
+        let provinciasSelect = document.getElementById('provincias');
 
-            provinciasOption.innerHTML = key;
-            provinciasSelect.appendChild(provinciasOption);
+        provinciasOption.innerHTML = key;
+        provinciasSelect.appendChild(provinciasOption);
 
+    }
+
+    document.querySelector('#provincias').addEventListener('change', function () {
+        let localidades = jsObj[this.value]
+
+        localidadesSelect.length = 0;
+
+        for (const iterator of localidades) {
+
+            let localidadesOption = document.createElement('option');
+            let localidadesSelect = document.getElementById('localidades');
+
+            localidadesOption.innerHTML = iterator;
+            localidadesSelect.appendChild(localidadesOption);
 
         }
-        document.querySelector('#provincias').addEventListener('change', function () { 
-            let localidades = jsObj[this.value]
-
-            localidadesSelect.length = 0;
-
-            for (const iterator of localidades) {
-
-                let localidadesOption = document.createElement('option');
-                let localidadesSelect = document.getElementById('localidades');
-
-                localidadesOption.innerHTML = iterator;
-                localidadesSelect.appendChild(localidadesOption);
-
-            }
-        });
     });
+
