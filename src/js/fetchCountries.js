db48774aaaa579ac86f04362cd2f2f5ca3b4ca70
Wences81 <zhylinpochta@gmail.com>
2021-10-21 18:53:23 +0200

    start

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountry(name) {
+    return fetch(`${BASE_URL}/name/${name}`).then((response) => 
+            response.json(),
+        );        
+}
+
+export default { fetchCountry };
+
+
