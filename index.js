ef5cbee0a1ecc0df3c5d5c83651507dfa6cd78cd
Wences81 <zhylinpochta@gmail.com>
2021-10-23 20:55:45 +0200

    cont5

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-const BASE_URL = 'http://sitelicon.eu/test/ajax_localidades.php';
+function fetchProvincias(name) {
 
-function fetchProvincias(optoin) {
-    return fetch(`${BASE_URL}/option/${option}`).then((response) =>
-        response.json(),
+    return fetch(`http://sitelicon.eu/test/ajax_localidades.php/name/${name}`, options)
+        .then(res => res.json())
+        .then(json => {
 
-    );
-   
 
-    
+            let jsObj = json
+            for (let key in jsObj) {
 
+                let provinciasOption = document.createElement('option');
+                let provinciasSelect = document.getElementById('provincias');
+
+                provinciasOption.innerHTML = key;
+                provinciasSelect.appendChild(provinciasOption);
+
+            }
+
+            document.querySelector('#provincias').addEventListener('change', function () {
+                let localidades = jsObj[this.value]
+
+                localidadesSelect.length = 0;
+
+                for (const iterator of localidades) {
+
+                    let localidadesOption = document.createElement('option');
+                    let localidadesSelect = document.getElementById('localidades');
+
+                    localidadesOption.innerHTML = iterator;
+                    localidadesSelect.appendChild(localidadesOption);
+
+                }
+            });
+        });
 }
-        let jsObj = json
-        for (let key in jsObj) {
-
-        let provinciasOption = document.createElement('option');
-        let provinciasSelect = document.getElementById('provincias');
-
-        provinciasOption.innerHTML = key;
-        provinciasSelect.appendChild(provinciasOption);
-
-    }
-
-    document.querySelector('#provincias').addEventListener('change', function () {
-        let localidades = jsObj[this.value]
-
-        localidadesSelect.length = 0;
-
-        for (const iterator of localidades) {
-
-            let localidadesOption = document.createElement('option');
-            let localidadesSelect = document.getElementById('localidades');
-
-            localidadesOption.innerHTML = iterator;
-            localidadesSelect.appendChild(localidadesOption);
-
-        }
-    });
 
